6295c235566bf3a28b7df0a231418a71f7d81fa0
sandeep7722 <sandeepsaroj167@gmail.com>
2020-08-03 09:25:38 +0530

    first commit

---
+++ js/quiz.js
@@ -0,0 +1,117 @@
+window.onload=function()
+{
+	show(0);
+
+}
+
+
+let questions=[
+	{
+		id: 1,
+		question: "what is the full form of RAM.?",
+		answer: "Random Access Memory",
+		options: [
+			"Random Access Memory",
+			"Randomly Access Memory",
+			"Run Accept Memory",
+			"None of these"
+		]
+	},
+	{
+		id: 2,
+		question: "what is the full form of CPU.?",
+		answer: "Central Processing Unit",
+		options: [
+			"Central Program Unit",
+			"Central Processing Unit",
+			"Central Preload Unit",
+			"None of these"
+		]
+	},
+	{
+		id: 3,
+		question: "what is the full form of E-mail.?",
+		answer: "Electronic Mail",
+		options: [
+			"Electrical Mail",
+			"Electronic Mail",
+			"Engine Mail",
+			"None of these"
+		]
+	}
+
+
+];
+
+
+
+function submitform(e) {
+	e.preventDefault();
+	let name=document.forms["welcome_form"]["name"].value;
+	sessionStorage.setItem("name",name);
+	location.href="file:///C:/Users/sande/Documents/html/quizgame/quizq.html";
+	console.log(name);
+}
+let question_count=0;
+let point=0;
+function next()
+{
+	let user_answer=document.querySelector("li.option.active").innerHTML;
+	//console.log(user_answer);
+	if (user_answer==questions[question_count].answer) {
+		//console.log("right answer!");
+		point+=10;
+		sessionStorage.setItem("points",point);
+	}
+	if (question_count==questions.length-1) {
+		sessionStorage.setItem("time",`${minutes} minutes and ${seconds} seconds`)
+		clearInterval(mytime);
+		location.href="file:///C:/Users/sande/Documents/html/quizgame/end.html";
+		
+		return;
+
+
+	}
+	
+	// else
+	// {
+	// 	console.log("wrong answer!");
+	// }
+
+	question_count++;
+	show(question_count);
+
+}
+function show(count)
+{
+	let question=document.getElementById("questions");
+	//question.innerHTML="<h2>"+questions[count].question+"</h2>";
+	question.innerHTML=`
+	<h2>Q${question_count+1}.${questions[count].question}</h2>
+	<ul class="option_group">
+			<li class="option">${questions[count].options[0]}</li>
+			<li class="option">${questions[count].options[1]}</li>
+			<li class="option">${questions[count].options[2]}</li>
+			<li class="option">${questions[count].options[3]}</li>
+
+		</ul>
+	`;
+	toggleActive();
+
+}
+function toggleActive()
+{
+	let option=document.querySelectorAll("li.option");
+	for(let i=0;i<option.length;i++)
+	{
+		option[i].onclick=function()
+		{
+			for (let j = 0; j < option.length; j++) {
+				if (option[j].classList.contains("active")) {
+					option[j].classList.remove("active");
+				}
+			}
+			option[i].classList.add("active");
+		}
+	}
+}
